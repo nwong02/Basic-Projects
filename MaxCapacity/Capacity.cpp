adbedf3ed73f0b84d2b0bbd6a6a8eddf1eed60b2
Nelson Wong <nelsonwong107@gmail.com>
2021-08-03 16:40:09 -0400

    Project comments added.

--- MaxCapacity/Capacity.cpp
+++ MaxCapacity/Capacity.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*
+* Project		: Basic Projects
+* Author		: Nelson Wong
+* Title			: Capacity.cpp
+* Description	: Determine whether a truck with a maximum capacity can be loaded further or not.
+*/
+
 /*
 * Write a program that determines whether a truck is loaded beyond its rated capacity. The program will read in the truck's maximum load capacity, the number of boxes it's carrying, and the average weight of a box. If the total weight of the boxes is less than or equal to the truck's capacity, the program announces that the truck can legally carry all boxes and tells how many additional boxes can be added. If the number of boxes exceeds the maximum capacity, the program announces that the truck is overloaded and says how many must be removed.
 */
